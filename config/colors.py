2c2aca0c214ba26c291c1b2a47b5b480e01e7cbd
Schiz0idCat <agustin.guzman.v@proton.me>
2025-05-20 19:06:08 -0400

    random color

--- config/colors.py
+++ config/colors.py
@@ -1,3 +1,8 @@
+import random
+
+def rand():
+    return (random.randint(60, 200), random.randint(60, 200), random.randint(60, 200))
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (80, 80, 80)
